397555eb151edb2ad0c3c438a6dbee9b78dd79e9
ACU <antoniocurri@computer-antonio-curri.local>
2015-12-05 17:56:55 +0100

    Initial Commit

---
+++ SuperCool-beta/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperCool-beta
+//
+//  Created by ACU on 05/12/15.
+//  Copyright © 2015 ACU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuperCool-beta/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SuperCool-beta
+//
+//  Created by ACU on 05/12/15.
+//  Copyright © 2015 ACU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
